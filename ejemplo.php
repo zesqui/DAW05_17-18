388cea5d72d9e7a42ccca7f030a85bc68693c34a
“josefranciscocastillejomolina” <goru21@hotmail.com>
2018-05-08 17:43:08 +0200

    Actualización de los archivos README.md y ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -1,12 +1,47 @@
 <?php
+/**
+*
+* El archivo ejemplo.php es un archivo que contiene dos
+* funciones y una clase para multiplicar unos números
+*
+*/
+
+/**
+ *
+ * Función para multiplicar los tres números
+ * Esta función retorna la multiplicación de tres
+ * números recibidos
+ *
+ * @param type $v1 Primer número recibido
+ * @param type $v2 Segundo número recibido
+ * @param type $v3 Tercer número recibido
+ *
+ * @return type Retorna la multiplicación
+ *
+ */
 function multiplicarTresNumeros ($v1, $v2, $v3)
 {
 	return $v1*$v2*$v3;
 }
+/**
+ * Clase ejemplo
+ * 
+ * Esta clase contiene una función 
+ * que multiplica los números recibidos
+ * 
+ * @author José Francisco Castillejo
+ */
 class Ejemplo {
 	var $a;
 	var $b;	
   	var $c;
+	/**
+         * Función que llama a la función
+	 * multiplicarTresNumeros con los
+	 * números recibidos en la clase
+	 *
+         * @return type multiplicarTresNumeros 
+         */
 	function multiplicar ()
 	{
 		return multiplicarTresNumeros($this->a, $this->b, $this->c);
