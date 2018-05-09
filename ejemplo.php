088245dd2c3b0e24ad6f1fdd5618a4cbe68042e3
“josefranciscocastillejomolina” <goru21@hotmail.com>
2018-05-09 18:18:26 +0200

    EDICIÓN ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -9,14 +9,15 @@
 /**
  *
  * Función para multiplicar los tres números
+ *
  * Esta función retorna la multiplicación de tres
  * números recibidos
  *
- * @param type $v1 Primer número recibido
- * @param type $v2 Segundo número recibido
- * @param type $v3 Tercer número recibido
+ * @param int $v1 Primer
+ * @param int $v2 Segundo
+ * @param int $v3 Tercer
  *
- * @return type Retorna la multiplicación
+ * @return int Retorna la multiplicación
  *
  */
 function multiplicarTresNumeros ($v1, $v2, $v3)
@@ -32,8 +33,20 @@ function multiplicarTresNumeros ($v1, $v2, $v3)
  * @author José Francisco Castillejo
  */
 class Ejemplo {
+	/**
+	 * Variable para el primer número
+	 *
+	 */
 	var $a;
+	/**
+	 * Variable para el segundo número
+	 *
+	 */
 	var $b;	
+	/**
+	 * Variable para el tercer número
+	 *
+	 */
   	var $c;
 	/**
          * Función que llama a la función
